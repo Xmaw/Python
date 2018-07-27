ded37a2f3e6e56001b45eff5d703c3b29e8ffd9b
Xmaw <ej222up@student.lnu.se>
2018-07-27 21:56:19 +0200

    Code correction TODO from Folkol

--- FlashCards.py
+++ FlashCards.py
@@ -1,4 +1,5 @@
-from PyQt5 import QtWidgets, Qt
+from PyQt5 import QtWidgets
+from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QLabel
 import sys
 from random import shuffle
@@ -26,50 +27,39 @@ import docx2txt
 
 class Deck:
     def __init__(self):
-        self.deck = []
-        self.size = 0
+        self.cards = []
 
         # ----Get list elements from add_cards function----
-        self.add_cards()
+        self.file = "C:\\Users\\Elias\\Desktop\\School\\Nätverk\\FlashCards_Network.docx"
+        self.load_from_file(self.file)
 
-    def get_card(self, index):
-        return self.deck[index]
+    def get_card(self, top_card):
+        return self.cards[top_card]
 
-    def add_cards(self):
-        my_text = docx2txt.process("C:\\Users\\Elias\\Desktop\\School\\Nätverk\\FlashCards_Network.docx")
-        s = my_text.split("\n\n")
-        s_temp = ""
-        for word in s:
-            s_temp += word + "\n"
-
-        s1_temp = s_temp.split("\n")
-
-        for word in s1_temp:
-            s_temp = word.split('-')
+    def load_from_file(self, file):
+        my_text = docx2txt.process(file)
+        for excerpt in my_text.split("\n\n"):
+            s_temp = excerpt.split('-')
             if len(s_temp) == 2:
-                self.deck.append(Cards(s_temp[0], s_temp[1]))
-                self.size += 1
-
-    def print_size(self):
-        print(self.size)
+                self.cards.append(Cards(s_temp[0], s_temp[1]))
 
     def remove_card(self, index):
-        del self.deck[index]
+        del self.cards[index]
 
     def shuffle(self):
-        shuffle(self.deck)
+        shuffle(self.cards)
 
 
 class Cards:
-    def __init__(self, frontside, backside):
-        self.frontside = frontside
-        self.backside = backside
+    def __init__(self, font, back):
+        self.font = font
+        self.back = back
 
-    def get_frontside(self):
-        return self.frontside
+    def get_front(self):
+        return self.font
 
-    def get_backside(self):
-        return self.backside
+    def get_back(self):
+        return self.back
 
 
 class Window(QtWidgets.QWidget):
@@ -79,8 +69,6 @@ class Window(QtWidgets.QWidget):
         self.b_show = QtWidgets.QPushButton('Show')
         self.b_no = QtWidgets.QPushButton('No')
         self.b_yes = QtWidgets.QPushButton('Yes')
-        self.list_word = []
-        self.list_explanation = []
 
         # ----H_BOX to store the widgets in ----
         self.h_box = QtWidgets.QVBoxLayout()
@@ -91,8 +79,7 @@ class Window(QtWidgets.QWidget):
         self.word_label.setAlignment(Qt.AlignCenter)
         self.word_label.setStyleSheet("font: 20pt;")
         self.word_label.setWordWrap(True)
-        print()
-        self.word_label.setText(deck.get_card(0).get_frontside())
+        self.word_label.setText(deck.get_card(0).get_front())
 
         self.setup_ui()
 
@@ -112,28 +99,29 @@ class Window(QtWidgets.QWidget):
         self.show()
 
     def b_show_click(self):
-        if not self.show_flag:
-            self.word_label.setText(deck.get_card(0).get_backside())
-            self.show_flag = True
-        elif self.show_flag:
-            self.word_label.setText(deck.get_card(0).get_frontside())
+        if self.show_flag:
+            self.word_label.setText(deck.get_card(0).get_back())
             self.show_flag = False
+        else:
+            self.word_label.setText(deck.get_card(0).get_front())
+            self.show_flag = True
 
     def b_yes_click(self):
-        completed.append(deck.get_card(0))  # Adds the card to the list of uncompleted FlashCards.
+        completed.append(deck.get_card(0))
         deck.remove_card(0)
-        self.word_label.setText(deck.get_card(0).get_frontside())
+        self.word_label.setText(deck.get_card(0).get_front())
 
     def b_no_click(self):
         deck.shuffle()
-        self.word_label.setText(deck.get_card(0).get_frontside())
+        self.word_label.setText(deck.get_card(0).get_front())
 
 
-deck = Deck()  # Creates the deck containing cards filled with information from a .docx file.
-uncompleted = []
-completed = []
+if __name__ == '__main__':
+    deck = Deck()  # Creates the deck containing cards filled with information from a .docx file.
+    uncompleted = []
+    completed = []
 
-app = QtWidgets.QApplication(sys.argv)
-a_window = Window()
-a_window.resize(700, 700)
-sys.exit(app.exec())
+    app = QtWidgets.QApplication(sys.argv)
+    a_window = Window()
+    a_window.resize(700, 700)
+    sys.exit(app.exec())
